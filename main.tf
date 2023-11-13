96a10362369b2794715d3cadd987856d51193bac
Prasanna-source31 <139847095+Prasanna-source31@users.noreply.github.com>
2023-11-13 20:22:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -141,3 +141,4 @@ resource "azurerm_log_analytics_workspace" "example" {
   name                = "example-log-analytics-workspace"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource
+}
